f53c84b87edfa51c724fad4647b0d16801ae9c23
pawan7295 <125091349+pawan7295@users.noreply.github.com>
2025-03-05 23:24:50 +0530

    Time: 14 ms (14.55%) | Memory: 20.5 MB (21.30%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    bool canFinish(int num, vector<vector<int>>& prerequisites) {
+        //creating graph
+        vector<int> adj[num];
+        for(auto it : prerequisites)
+        {
+            adj[it[1]].push_back(it[0]);
+        }
+
+    vector<int> indegree(num,0);
+    for(int i=0;i<num;i++)
+    {
+        for(auto neighbour : adj[i])
+        {
+            //i->neighbour
+            indegree[neighbour]++;
+        }
+    }
+    queue<int> qu;
+    unordered_set<int> vis;
+    for(int i=0;i<num;i++)
+    {
+        if(indegree[i]==0)
+        {
+            qu.push(i);
+            vis.insert(i);
+        }
+    }
+    vector<int> ans;
+    while(qu.size()>0)
+    {
+        int node=qu.front();
+        ans.push_back(node);
+        qu.pop();
+        for(auto neighbour : adj[node])
+        {
+             if(!vis.count(neighbour))
+             {
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0)
+                {
+                    qu.push(neighbour);
+                    vis.insert(neighbour);
+                }
+            }
+        }
+    }
+    if(ans.size()==num)
+    return true;
+    else
+    return false;
+    }
+};
