705c457d7db0bccf0d92f793b454ce9948dc9f2d
Pawan Kumar Yadav <125091349+pawan7295@users.noreply.github.com>
2025-07-01 13:19:54 +0530

    Time: 3 ms (31.46%) | Memory: 16.3 MB (39.92%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+int cost(vector<vector<int>>& grid,int i,int j,vector<vector<int>> &dp)
+{
+     int r=grid.size();
+     int c=grid[0].size();
+    if(i>=r || j>=c) return INT_MAX;
+    if(i==r-1 && j==c-1) return grid[i][j];
+    if(dp[i][j]!=-1) return dp[i][j];
+    int right=cost(grid,i+1,j,dp);
+    int down=cost(grid,i,j+1,dp);
+    dp[i][j]=grid[i][j]+min(right,down);
+    return dp[i][j];
+}
+    int minPathSum(vector<vector<int>>& grid) {
+       int r=grid.size();
+       int c=grid[0].size();
+       vector<vector<int>> dp(r,vector<int> (c,-1));
+       return cost(grid,0,0,dp);
+    }
+};
